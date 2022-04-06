13268df41f6e108422bee24b30d3f3325cc9a772
choen0 <schoen02@hhu.de>
2022-04-06 15:28:20 +0200

    commit

--- jenkDebug.bat
+++ jenkDebug.bat
@@ -10,3 +10,5 @@ cd build
 call conan install .. -s build_type=Debug -s compiler="Visual Studio" -s compiler.runtime=MDd --build=missing
 call cmake .. -G "Visual Studio 17 2022"
 call cmake --build . --config Debug 
+
+call build/bin/conan_example.exe
