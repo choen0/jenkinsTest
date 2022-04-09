17133a0ddade70595a93ab2f03b6026307db2d68
Ben Schoenherr <schoen@hhu.de>
2022-04-09 12:31:12 +0200

    commited

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main()
     };
 
     std::cout << "We got here" << std::to_string((int) json["pi"]) << std::endl;
-    fmt::print("yaaasaa {}", 13);
+    fmt::print("yaaaasaa {}", 13);
     std::cin >> k;
     return 0;
 }
